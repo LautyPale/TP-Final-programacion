a061b4cdc2d3881bdbc5a4fef1c0641646d4d39c
PalenaLautaro <palenalautaro@gmail.com>
2022-06-06 11:54:35 -0300

    defino structs

--- main.c
+++ main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define
+
+typedef struct{
+char nombre[30];
+char apellido[30];
+unsigned char mail[40]; /// 0 a 255 ASCII | especificador de formato: %c
+unsigned long int telefono; /// 0 a 4,294,967,295 | especificador de formato: %lu
+int dni;
+char calleDireccion[50];
+int calleNumero;
+int idcliente;
+int bajacliente;
+}stCliente;
+
+typedef struct{
+char nombreproducto[30];
+int idproducto;
+float precioproducto;
+}stProducto;
+
+typedef struct{
+char descripcion[70];
+float costopedido;
+int idpedido;
+char fechapedido[10];
+int pedidoanulado;
+idcliente
+}stPedido;
 
 int main()
 {
-    printf("Hello world!\n");
+    stCliente aux;
+    aux = CargarUnCliente (aux);
+    MostrarUnCliente (aux);
+
+
     return 0;
 }
